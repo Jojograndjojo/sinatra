27081098e65f6d92a0be98afb941d91f47f392c4
Stephen Dawes <sdawes@outlook.com>
2016-06-06 16:08:40 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "WTF is this??"
+end
+
+get '/dawes' do
+  "Stephen Dawes is struggling with this"
+end
+
+get '/wtf' do
+  "OK, I am very very slowly getting this"
+end
