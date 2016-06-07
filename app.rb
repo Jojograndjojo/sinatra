26470605838965fd3d2cf3a32a41cb7e2616d7ba
Jonathan <j_passe@live.fr>
2016-06-07 12:15:36 +0100

    Chap 07

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "I can't tell you"
+end
+
+get '/Michael' do
+  "Random stuff !!!"
 end
